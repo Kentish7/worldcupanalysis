d49bff9f6132bcb7e7015a527a944d834487490f
Luis Rodrigues <luishenriquesc2020@gmail.com>
2023-01-21 22:45:45 -0300

    Countries that has more young player titles

---
+++ queries/youngPlayers.sql
@@ -0,0 +1,9 @@
+SELECT 
+team_name AS 'Country Name', 
+award_name AS 'Award Name', 
+COUNT(award_name) AS 'Quantity' 
+FROM award_winners 
+WHERE award_name = 'Best Young Player' 
+GROUP BY team_name 
+ORDER BY Quantity DESC 
+LIMIT 5;
